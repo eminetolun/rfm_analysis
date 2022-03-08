24f92d42e2246cb4027b56a4be1104ab687fd495
eminetolun <eminetolun0@gmail.com>
2022-03-08 14:45:37 +0300

    Update rfm.py

--- rfm.py
+++ rfm.py
@@ -30,7 +30,9 @@ df.head()
 
 
 df["InvoiceDate"].max()
-today_date =dt.datetime(2010,12,11)
+
+today_date =dt.datetime(2011,12,11)
+#  Timestamp('2011-12-09 12:50:00')
 
 rfm = df.groupby("Customer ID").agg({ "InvoiceDate": lambda InvoiceDate:(today_date - InvoiceDate.max()).days,
                                 "Invoice": lambda Invoice: Invoice.nunique(),
@@ -47,6 +49,7 @@ rfm.columns = ["recency", "frequency", "monetary"]
 rfm["recency_score"] = pd.qcut(rfm["recency"], 5, labels=[5,4,3,2,1])
 rfm["frequency_score"] = pd.qcut(rfm["frequency"].rank(method = "first"),5 ,labels=[1,2,3,4,5])
 rfm["monetary_score"] = pd.qcut(rfm["monetary"], 5, labels=[1,2,3,4,5])
+
 rfm.head()
 
 rfm["RFM_SCORE"] =  (rfm["recency_score"].astype(str) + rfm["frequency_score"].astype(str))
@@ -65,6 +68,9 @@ seg_map = {
 }
 
 rfm["segment"] = rfm["RFM_SCORE"].replace(seg_map,regex = True)
+
 rfm = rfm[["recency", "frequency", "monetary", "segment"]]
+
 rfm.head()
+
 
